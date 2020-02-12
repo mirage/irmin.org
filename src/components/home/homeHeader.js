97fd1bed7896dcc6eaca3209adebf7e3027d278a
Clément Pascutto <clement@tarides.com>
2020-02-12 11:24:35 +0100

    Add Irmin CI badge

--- src/components/home/homeHeader.js
+++ src/components/home/homeHeader.js
@@ -14,6 +14,13 @@ const HomeHeader = ({ pages, currentLink }) => {
           <img className="logo" src={imgLogo} alt="Irmin logo" />
         </Link>
         <h1>A distributed database built on the same principles as Git</h1>
+        <a href="https://ci.ocamllabs.io/github/mirage/irmin" className="build">
+          <img
+            src="https://img.shields.io/endpoint?url=https%3A%2F%2Fci.ocamllabs.io%2Fbadge%2Fmirage%2Firmin%2Fmaster&logo=ocaml\&style=for-the-badge"
+            alt="OCaml-CI Status"
+          />
+        </a>
+        <br />
         <a href="https://github.com/mirage/irmin" className="button github">
           <img src={imgGithub} alt="GitHub" />
           GitHub
