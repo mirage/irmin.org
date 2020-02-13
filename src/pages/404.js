1cee2bb406c637d8b8f583923ee57b5147734e66
Clément Pascutto <clement@tarides.com>
2020-02-13 13:17:47 +0100

    Fix incorrect use of `class`

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 
 const NotFoundPage = () => (
   <Layout title="404: Not Found">
-    <main class="wrapper">
+    <main className="wrapper">
       <h2>NOT FOUND</h2>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </main>
