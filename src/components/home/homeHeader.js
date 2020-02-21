7fd071ee40e6b3647d9131dcf5302d0ea94739c2
Romain Liautaud <romain@liautaud.fr>
2020-02-21 18:19:17 +0100

    Generalize the .button styles to any tag.

--- src/components/home/homeHeader.js
+++ src/components/home/homeHeader.js
@@ -14,7 +14,7 @@ const HomeHeader = ({ pages, currentLink }) => {
           <img className="logo" src={imgLogo} alt="Irmin logo" />
         </Link>
         <h1>A distributed database built on the same principles as Git</h1>
-        <a href="https://github.com/mirage/irmin" className="button github">
+        <a href="https://github.com/mirage/irmin" className="button light github">
           <img src={imgGithub} alt="GitHub" />
           GitHub
         </a>
